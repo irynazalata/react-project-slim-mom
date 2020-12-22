ffef0dc1b224387408119fb454ccf78c22575670
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-12-22 22:29:02 +0200

    Navigator (#22)
    * routes
    
    * comments from tieam leader
    
    Co-authored-by: Iryna Zalata <68591872+irynazalata@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import routes from "./routes";
+import { authOperations } from "../redux/auth";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 import Header from "./components/Header/Header";
-import Logo from "./components/Logo/Logo.js";
+import Loader from "./components/Loader/Loader";
 import Modal from './shared/Modal/Modal';
 import "./App.css";
 
 class App extends Component {
-  state = {
+   state = {
     showModal: false,
   };
   modalToggle = () => {
     this.setState((prevState) => ({ showModal: !prevState.showModal }));
   };
+  componentDidMount() {
+    this.props.onGetCurrentUser();
+  }
   render() {
     return (
-      <>
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            {routes.map((route) =>
+              route.private ? (
+                <PrivateRoute key={route.label} {...route} />
+              ) : (
+                <PublicRoute key={route.label} {...route} />
+              )
+            )}
+          </Switch>
       <div className="headerContainer">
         <Header />
       </div>
       <div className="bottomVectorOfHeader"></div>
-        <h1>Hello world</h1>
         {this.state.showModal && <Modal onModalToggle={this.modalToggle} />}
-      </>
+        </Suspense>
+      </BrowserRouter>
     );
   }
 }
-export default App;
+
+export default connect(null, {
+  onGetCurrentUser: authOperations.getCurrentUser,
+})(App);
