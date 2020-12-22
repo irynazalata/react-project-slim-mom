c9e417101aa298769a91e25ea707a8aaa66f798d
Aleksey Gelich <alexeygelich@gmail.com>
2020-12-22 15:39:00 +0200

    логика регистрации

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import RegistrationForm from "./components/RegistrationForm/RegistrationFormContainer";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello world</h1>
+      <RegistrationForm />
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
