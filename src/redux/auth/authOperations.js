3bfd40aad290f6ba33c3abcb17a69ad32c0e956e
alexeygelich <65009861+alexeygelich@users.noreply.github.com>
2020-12-22 17:39:12 +0200

    логика логинизации (#17)
    * логика логинизации
    
    * убрал консоль логи
    
    * fix
    
    Co-authored-by: Iryna Zalata <68591872+irynazalata@users.noreply.github.com>

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,6 +3,15 @@ import authActions from "./authActions";
 
 axios.defaults.baseURL = "https://slimmom-backend.herokuapp.com/";
 
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
+};
+
 const register = (credentials) => (dispatch) => {
   dispatch(authActions.registerRequest());
 
@@ -14,6 +23,40 @@ const register = (credentials) => (dispatch) => {
     .catch((error) => dispatch(authActions.registerError(error)));
 };
 
+const login = (credentials) => (dispatch) => {
+  dispatch(authActions.loginRequest());
+
+  axios
+    .post("auth/login", credentials)
+    .then(({ data }) => {
+      token.set(data.accessToken);
+      dispatch(authActions.loginSuccess(data));
+    })
+    .catch((error) => dispatch(authActions.loginError(error)));
+};
+
+const getCurrentUser = () => (dispatch, getState) => {
+  const {
+    auth: { token: persistedToken },
+  } = getState();
+
+  if (!persistedToken) {
+    return;
+  }
+
+  token.set(persistedToken);
+  dispatch(authActions.getCurrentUserRequest());
+
+  axios
+    .get("user")
+    .then(({ data }) => {
+      dispatch(authActions.getCurrentUserSuccess(data));
+    })
+    .catch((error) => dispatch(authActions.getCurrentUserError(error)));
+};
+
 export default {
   register,
+  login,
+  getCurrentUser
 };
