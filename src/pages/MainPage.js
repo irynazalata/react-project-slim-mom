23b054d47d3ad94adcfec883ff9274bdba23e6fe
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-23 23:59:17 +0200

    styles corrections

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import DailyCaloriesForm from '../components/DailyCaloriesForm/DailyCaloriesForm';
 import Modal from '../shared/Modal/Modal.js';
 import Header from '../components/Header/Header';
-import { pageContainer } from './MainPage.module.css';
+import { pageContainer, bgContainer } from './MainPage.module.css';
 
 class MainPage extends Component {
   state = {
@@ -15,10 +15,12 @@ class MainPage extends Component {
   render() {
     return (
       <>
-        <Header />
-        <div className={pageContainer}>
-          <DailyCaloriesForm onShowModal={this.modalToggle} />
-          {this.state.showModal && <Modal onModalToggle={this.modalToggle} />}
+        <div className={bgContainer}>
+          <Header />
+          <div className={pageContainer}>
+            <DailyCaloriesForm onShowModal={this.modalToggle} />
+            {this.state.showModal && <Modal onModalToggle={this.modalToggle} />}
+          </div>
         </div>
       </>
     );
