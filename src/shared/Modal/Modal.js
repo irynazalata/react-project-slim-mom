9b4601a88e3617e5a2110fc62c37bff3a323d221
Natali Baziuchenko <66207144+Natali-bc@users.noreply.github.com>
2020-12-24 13:39:44 +0200

    Commit (#46)
    Co-authored-by: Iryna Zalata <68591872+irynazalata@users.noreply.github.com>

--- src/shared/Modal/Modal.js
+++ src/shared/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from '../Modal/Modal.module.css';
+import closeModalBtn from '../../images/close-burger-menu.png';
 import DailyCalorieIntake from '../../components/DailyCalorieIntake/DailyCalorieIntake';
 
 class Modal extends Component {
@@ -36,7 +37,11 @@ class Modal extends Component {
               className={styles.closeModalBtn}
               type="button"
             >
-              X
+              <img
+                src={closeModalBtn}
+                alt="close-modal"
+                className={closeModalBtn}
+              />
             </button>
             <DailyCalorieIntake />
           </div>
