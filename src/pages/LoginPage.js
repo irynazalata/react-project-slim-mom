0f6d75b4f90990c47dc29d02b307596deeb4b206
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-24 12:46:05 +0200

    correction (#44)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm/LoginFormContainer';
 import Header from '../components/Header/Header';
-import { pageContainer, bgContainer } from './LoginRegistrationPage.module.css';
+import Logo from '../components/Logo/Logo';
+import LoginForm from '../components/LoginForm/LoginFormContainer';
+import { pageContainer, bgContainer, headerHide, logoHide } from './LoginRegistrationPage.module.css';
 
 const LoginPage = () => (
     <>
-        <div className={bgContainer}>
+    <div className={bgContainer}>
+      <div className={headerHide}>
           <Header />
+      </div>
+            <div className={logoHide}>
+          <Logo />
+      </div>
         <div className={pageContainer}>
           <LoginForm />
         </div>
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Logo from '../components/Logo/Logo';
 import RegistrationForm from '../components/RegistrationForm/RegistrationFormContainer';
-import { pageContainer, bgContainer } from './LoginRegistrationPage.module.css';
+import { pageContainer, bgContainer, headerHide, logoHide } from './LoginRegistrationPage.module.css';
+
 
 
 const RegistrationPage = () => (
     <>
       <div className={bgContainer}>
-        <Header />  
+      <div className={headerHide}>
+          <Header />
+      </div>
+            <div className={logoHide}>
+          <Logo />
+      </div>
         <div className={pageContainer}>
           <RegistrationForm />
         </div>
