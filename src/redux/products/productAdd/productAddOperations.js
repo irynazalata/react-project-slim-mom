c5f3a42204777c7d9adfbf40c025cb68cad4a6e8
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-12-24 10:51:12 +0200

    изменяет ссылку на бек

--- src/redux/products/productAdd/productAddOperations.js
+++ src/redux/products/productAdd/productAddOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import productAddActions from './productAddActions.js';
 
-axios.defaults.baseURL = 'http://slimmom-backend.herokuapp.com';
+axios.defaults.baseURL = 'https://slimmom-backend.goit.global/';
 
 const addProduct = (date, productId, weight) => dispatch => {
   dispatch(productAddActions.addProductRequest())
