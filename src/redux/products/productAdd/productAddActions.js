493e4dfafcf3a92b041b43930ac3e797f7eb1c5e
Iryna Zalata <iryna.zalata@gmail.com>
2020-12-25 10:00:35 +0200

    Added the list of the products

--- src/redux/products/productAdd/productAddActions.js
+++ src/redux/products/productAdd/productAddActions.js
@@ -1,6 +1,4 @@
 import { createAction } from '@reduxjs/toolkit';
-;
-
 const addProductRequest = createAction('Product/addRequest');
 const addProductSuccess = createAction('Product/addSuccess');
 const addProductError = createAction('Product/addError');
@@ -12,6 +10,10 @@ const fetchProductError = createAction('Product/fetchError');
 // const searchProductRequest = createAction('Product/searchRequest');
 // const searchProductSuccess = createAction('Product/searchSuccess');
 // const searchProductError = createAction('Product/searchError');
+
+const deleteProductRequest = createAction('DELETE_PRODUCT_REQUEST');
+const deleteProductSuccess = createAction('DELETE_PRODUCT_SUCCESS');
+const deleteProductError = createAction('DELETE_PRODUCT_ERROR');
 export default {
   addProductRequest,
   addProductSuccess,
@@ -22,4 +24,4 @@ export default {
   // searchProductRequest,
   // searchProductSuccess,
   // searchProductError
-}
+};
