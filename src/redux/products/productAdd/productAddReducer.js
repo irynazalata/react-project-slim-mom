bf3c1e062afad4acf8fc62bfa2084609da99f9a7
Iryna Zalata <68591872+irynazalata@users.noreply.github.com>
2020-12-25 17:36:18 +0200

    Products fetch remove (#65)
    * Added the list of the products
    
    * Added removal of the product

--- src/redux/products/productAdd/productAddReducer.js
+++ src/redux/products/productAdd/productAddReducer.js
@@ -3,14 +3,20 @@ import { createReducer } from '@reduxjs/toolkit';
 import productAddActions from './productAddActions.js';
 
 const toAddProduct = (state, action) => {
-  return action.payload
-}
+  return action.payload;
+};
 
-const products = createReducer({}, {
-  [productAddActions.fetchProductSuccess]: (state, action) => action.payload,
-  [productAddActions.addProductSuccess]: toAddProduct,
-})
+const toDeleteProduct = (state, action) => {
+  return action.payload;
+};
 
+const products = createReducer(
+  {},
+  {
+    [productAddActions.fetchProductSuccess]: (state, action) => action.payload,
+    [productAddActions.addProductSuccess]: toAddProduct,
+    [productAddActions.deleteProductSuccess]: toDeleteProduct,
+  },
+);
 
-
-export default products
+export default products;
