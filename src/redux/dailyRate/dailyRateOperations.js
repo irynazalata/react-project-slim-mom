b7afa3df7b93689806329a02cd77a332e2f2de57
Julia-itcourse <juliaartemenko93@gmail.com>
2020-12-25 10:45:30 +0100

    adding error handling to dailyRateOperations

--- src/redux/dailyRate/dailyRateOperations.js
+++ src/redux/dailyRate/dailyRateOperations.js
@@ -19,17 +19,56 @@ const onFetchDailyRates = (values) => (dispatch) => {
 
 
   const onFetchDailyRatesAuthorised = (values, userId) => (dispatch) => {
-    dispatch(dailyRateActions.fetchDailyRateRequest())
+    dispatch(dailyRateActions.fetchDailyRateRequestAuth())
   
     axios
       .post(`/daily-rate/${userId}`, values)
       .then((receivedData) =>
-        dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data))
+        dispatch(dailyRateActions.fetchDailyRateSuccessAuth(receivedData.data))
       )
-      .catch((error) => dispatch(dailyRateActions.fetchDailyRateError(error)))
+      .catch((error) => dispatch(dailyRateActions.fetchDailyRateErrorAuth(error)))
   }
 
   export default {
     onFetchDailyRates,
     onFetchDailyRatesAuthorised
   }
+
+
+//*Before error handling
+
+// import axios from "axios"
+// import {useSelector} from "react-redux"
+// import dailyRateActions from "./dailyRateActions"
+// import authSelectors from "../auth/authSelectors"
+
+// axios.defaults.baseURL = "https://slimmom-backend.goit.global";
+
+// const onFetchDailyRates = (values) => (dispatch) => {
+//     dispatch(dailyRateActions.fetchDailyRateRequest())
+  
+//     axios
+//       .post("/daily-rate", values)
+//       .then((receivedData) =>
+//         {console.log(receivedData.data);
+//             dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data))}
+//       )
+//       .catch((error) => dispatch(dailyRateActions.fetchDailyRateError(error)))
+//   }
+
+
+//   const onFetchDailyRatesAuthorised = (values, userId) => (dispatch) => {
+//     dispatch(dailyRateActions.fetchDailyRateRequest())
+  
+//     axios
+//       .post(`/daily-rate/${userId}`, values)
+//       .then((receivedData) =>
+//         dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data))
+//       )
+//       .catch((error) => dispatch(dailyRateActions.fetchDailyRateError(error)))
+//   }
+
+//   export default {
+//     onFetchDailyRates,
+//     onFetchDailyRatesAuthorised
+//   }
