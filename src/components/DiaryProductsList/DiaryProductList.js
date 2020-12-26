1a63f1a0b915eb51125f81b8c7104a2275857b31
alexeygelich <65009861+alexeygelich@users.noreply.github.com>
2020-12-26 10:04:12 +0200

    loader (#67)
    Co-authored-by: Iryna Zalata <68591872+irynazalata@users.noreply.github.com>

--- src/components/DiaryProductsList/DiaryProductList.js
+++ src/components/DiaryProductsList/DiaryProductList.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import productAddSelectors from '../../redux/products/productAdd/productAddSelectors';
-import productAddOperations from '../../redux/products/productAdd/productAddOperations';
-import DiaryProductsListItem from './DiaryProductsListItem/DiaryProductsListItem';
-import styles from './DiaryProductsList.module.css';
-import { Component } from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import productAddSelectors from "../../redux/products/productAdd/productAddSelectors";
+import productAddOperations from "../../redux/products/productAdd/productAddOperations";
+import DiaryProductsListItem from "./DiaryProductsListItem/DiaryProductsListItem";
+import styles from "./DiaryProductsList.module.css";
+import { Component } from "react";
 
 class DiaryProductsList extends Component {
-  componentDidUpdate() {
+  componentDidMount() {
     this.props.toFetchProducts(this.props.date);
+  }
+
+  componentDidUpdate(prevProps, PrevState) {
+    const countPrev = prevProps.products ? prevProps.products.length : 0;
+    const countNow = this.props.products ? this.props.products.length : 0;
+
+    if (countPrev !== countNow) {
+      this.props.toFetchProducts(this.props.date);
+    }
   }
   render() {
     return (
       <>
         {!this.props.products || !(this.props.products.length > 0) ? (
-          <h4 className={styles.title}>
-            Съеденные в этот день продукты ещё не добавлены
-          </h4>
+          <h4 className={styles.title}>Съеденные в этот день продукты ещё не добавлены</h4>
         ) : (
-          ''
+          ""
         )}
         {this.props.products && (
           <ul className={styles.list}>
@@ -32,13 +39,13 @@ class DiaryProductsList extends Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   products: productAddSelectors.getProducts(state),
   date: state.date,
 });
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    toFetchProducts: date => dispatch(productAddOperations.fetchProducts(date)),
+    toFetchProducts: (date) => dispatch(productAddOperations.fetchProducts(date)),
   };
 };
 
